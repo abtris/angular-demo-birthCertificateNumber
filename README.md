ce20897c79971b92368f96eb779a9d81f0916b12
Ladislav Prskavec <ladislav@prskavec.net>
2012-10-30 19:50:16 +0100

    Step 3 - add application logic

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
-yeoman init angular 
+# Demo 
 
-- http://briantford.com/blog/angular-yeoman.html
+- yeoman init angular 
+- download boostrap and modify index.html, update html for demo
+- add moment libraty, add simple logic
+- @todo tests
 
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,9 +1,18 @@
 'use strict';
 
 demosAngularApp.controller('MainCtrl', function($scope) {
-  $scope.awesomeThings = [
-    'HTML5 Boilerplate',
-    'AngularJS',
-    'Testacular'
-  ];
+	$scope.change = function () {
+
+		var parts  = $scope.birthnumber.match(/[\s\S]{1,2}/g) || [];
+		if (parts[1]<=12) {
+			$scope.kind = "male";
+		} 
+		if (parts[1]>=51 && parts[1]<=62) {
+			$scope.kind = "female";	
+			parts[1] =	parts[1] - 50;
+		}
+
+		var day = moment(parts.join(''), "YYMMDD");
+		$scope.birth = day.format("dddd, MMMM Do YYYY");
+	}
 });
